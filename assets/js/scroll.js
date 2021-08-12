9108e6ae071e3c96dda19ded0d60dc8da6b2a8ad
= <danielmezauaeh@gmail.com>
2021-08-11 21:23:58 -0500

    Funcionalidad de scroll en el header terminado

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,1 +1,13 @@
-alert("Si funciona");
+const header = document.getElementById("main-header");
+
+function scrollHeaderTop() {
+  //   console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    header.classList.add("fixed-header");
+  } else if (window.scrollY === 0) {
+    header.classList.remove("fixed-header");
+  }
+}
+
+document.addEventListener("scroll", scrollHeaderTop);
+// document.onscroll = scrollHeaderTop;
